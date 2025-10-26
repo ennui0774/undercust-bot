213aba17eb423438877c343e88f68fde436140ae
ennui0774 <115736693+ennui0774@users.noreply.github.com>
2025-10-26 21:15:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -201,6 +201,12 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    import nest_asyncio
+    import asyncio
+
+    nest_asyncio.apply()
+    asyncio.get_event_loop().run_until_complete(main())
+
+
 
 
